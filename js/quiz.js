be434458a6ccb75e9277f87caead9f2d50cbe99d
hashchirag <chiragshenoy@hashlearn.com>
2016-02-03 15:04:12 +0530

    Latex done.

--- js/quiz.js
+++ js/quiz.js
@@ -1,25 +1,40 @@
 (function() {
 
   var app = angular.module('myQuiz', ['ngMaterial']);
+  app.directive('mathJaxBind', function() {
+    var refresh = function(element) {
+        MathJax.Hub.Queue(["Typeset", MathJax.Hub, element]);
+    };
+    return {
+      link: function(scope, element, attrs) {
+        scope.$watch(attrs.mathJaxBind, function(newValue, oldValue) {
+          element.text(newValue);
+          refresh(element[0]);
+        });
+      }
+    };
+  });
 
   app.controller('QuizController', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
 
     $scope.selected = [];
     $scope.totalQuestions = 3;
+    $scope.ended = true;
 
-    $scope.drawCanvas = (function drawCanvas(){
-      var canvas=document.getElementById('mycanvas');
-      var ctx=canvas.getContext('2d');
-      var cWidth=canvas.width;
-      var cHeight=canvas.height;
+    $scope.drawCanvas = (function drawCanvas() {
+      var canvas = document.getElementById('mycanvas');
+      var ctx = canvas.getContext('2d');
+      var cWidth = canvas.width;
+      var cHeight = canvas.height;
 
-      var countTo=2700;
+      $scope.countTo = 10;
+      $scope.counToTime = 10;
 
-      var min=Math.floor(countTo/60);
-      var sec=countTo-(min*60);
-      $scope.counter=0;
-      var angle=270;
-      var inc=360/countTo;
+      var min = Math.floor($scope.countTo / 60);
+      var sec = $scope.countTo - (min * 60);
+      $scope.counter = 0;
+      var angle = 270;
+      var inc = 360 / $scope.countTo;
 
 
       function drawScreen() {
@@ -28,83 +43,83 @@
 
         //======= reset canvas
 
-        ctx.fillStyle="#000000";
-        ctx.fillRect(0,0,cWidth,cHeight);
+        ctx.fillStyle = "#000000";
+        ctx.fillRect(0, 0, cWidth, cHeight);
 
         //========== base arc
 
         ctx.beginPath();
-        ctx.strokeStyle="#252424";
-        ctx.lineWidth=14;
-        ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*0,(Math.PI/180)*360,false);
+        ctx.strokeStyle = "#252424";
+        ctx.lineWidth = 14;
+        ctx.arc(cWidth / 2, cHeight / 2, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
         ctx.stroke();
         ctx.closePath();
 
         //========== dynamic arc
 
         ctx.beginPath();
-        ctx.strokeStyle="#df8209";
-        ctx.lineWidth=14;
-        ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*270,(Math.PI/180)*angle,false);
+        ctx.strokeStyle = "#df8209";
+        ctx.lineWidth = 14;
+        ctx.arc(cWidth / 2, cHeight / 2, 100, (Math.PI / 180) * 270, (Math.PI / 180) * angle, false);
         ctx.stroke();
         ctx.closePath();
 
         //======== inner shadow arc
 
-        grad=ctx.createRadialGradient(cWidth/2,cHeight/2,80,cWidth/2,cHeight/2,115);
-        grad.addColorStop(0.0,'rgba(0,0,0,.4)');
-        grad.addColorStop(0.5,'rgba(0,0,0,0)');
-        grad.addColorStop(1.0,'rgba(0,0,0,0.4)');
+        grad = ctx.createRadialGradient(cWidth / 2, cHeight / 2, 80, cWidth / 2, cHeight / 2, 115);
+        grad.addColorStop(0.0, 'rgba(0,0,0,.4)');
+        grad.addColorStop(0.5, 'rgba(0,0,0,0)');
+        grad.addColorStop(1.0, 'rgba(0,0,0,0.4)');
 
         ctx.beginPath();
-        ctx.strokeStyle=grad;
-        ctx.lineWidth=14;
-        ctx.arc(cWidth/2,cHeight/2,100,(Math.PI/180)*0,(Math.PI/180)*360,false);
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = 14;
+        ctx.arc(cWidth / 2, cHeight / 2, 100, (Math.PI / 180) * 0, (Math.PI / 180) * 360, false);
         ctx.stroke();
         ctx.closePath();
 
         //======== bevel arc
 
-        grad=ctx.createLinearGradient(cWidth/2,0,cWidth/2,cHeight);
-        grad.addColorStop(0.0,'#6c6f72');
-        grad.addColorStop(0.5,'#252424');
+        grad = ctx.createLinearGradient(cWidth / 2, 0, cWidth / 2, cHeight);
+        grad.addColorStop(0.0, '#6c6f72');
+        grad.addColorStop(0.5, '#252424');
 
         ctx.beginPath();
-        ctx.strokeStyle=grad;
-        ctx.lineWidth=1;
-        ctx.arc(cWidth/2,cHeight/2,93,(Math.PI/180)*0,(Math.PI/180)*360,true);
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = 1;
+        ctx.arc(cWidth / 2, cHeight / 2, 93, (Math.PI / 180) * 0, (Math.PI / 180) * 360, true);
         ctx.stroke();
         ctx.closePath();
 
         //====== emboss arc
 
-        grad=ctx.createLinearGradient(cWidth/2,0,cWidth/2,cHeight);
-        grad.addColorStop(0.0,'transparent');
-        grad.addColorStop(0.98,'#6c6f72');
+        grad = ctx.createLinearGradient(cWidth / 2, 0, cWidth / 2, cHeight);
+        grad.addColorStop(0.0, 'transparent');
+        grad.addColorStop(0.98, '#6c6f72');
 
         ctx.beginPath();
-        ctx.strokeStyle=grad;
-        ctx.lineWidth=1;
-        ctx.arc(cWidth/2,cHeight/2,107,(Math.PI/180)*0,(Math.PI/180)*360,true);
+        ctx.strokeStyle = grad;
+        ctx.lineWidth = 1;
+        ctx.arc(cWidth / 2, cHeight / 2, 107, (Math.PI / 180) * 0, (Math.PI / 180) * 360, true);
         ctx.stroke();
         ctx.closePath();
 
         //====== Labels
 
-        var textColor='#646464';
-        var textSize="12";
-        var fontFace="helvetica, arial, sans-serif";
+        var textColor = '#646464';
+        var textSize = "12";
+        var fontFace = "helvetica, arial, sans-serif";
 
-        ctx.fillStyle=textColor;
-        ctx.font=textSize+"px "+fontFace;
-        ctx.fillText('MIN',cWidth/2-46,cHeight/2-15);
-        ctx.fillText('SEC',cWidth/2+25,cHeight/2-15);
+        ctx.fillStyle = textColor;
+        ctx.font = textSize + "px " + fontFace;
+        ctx.fillText('MIN', cWidth / 2 - 46, cHeight / 2 - 15);
+        ctx.fillText('SEC', cWidth / 2 + 25, cHeight / 2 - 15);
 
         //====== Values
 
 
 
-        ctx.fillStyle='#6292ae';
+        ctx.fillStyle = '#6292ae';
 
         // if (min>9) {
         //   ctx.font='84px '+fontFace;
@@ -115,39 +130,47 @@
         // }
         // else {
         {
-          ctx.font='60px '+fontFace;
-          ctx.fillText(min ,cWidth/2-60,cHeight/2+35);
+          ctx.font = '60px ' + fontFace;
+          ctx.fillText(min, cWidth / 2 - 60, cHeight / 2 + 35);
         }
 
-        ctx.font='50px '+fontFace;
-        if (sec<10) {
-          ctx.fillText('0'+sec,cWidth/2+10,cHeight/2+35);
-        }
-        else {
-          ctx.fillText(":" + sec,cWidth/2+10,cHeight/2+35);
+        ctx.font = '50px ' + fontFace;
+        if (sec < 10) {
+          ctx.fillText('0' + sec, cWidth / 2 + 10, cHeight / 2 + 35);
+        } else {
+          ctx.fillText(":" + sec, cWidth / 2 + 10, cHeight / 2 + 35);
         }
 
 
-        if (sec<=0 && $scope.counter<countTo) {
-          angle+=inc;
+        if (sec <= 0 && $scope.counter < $scope.countTo) {
+          angle += inc;
           $scope.counter++;
           min--;
-          sec=59;
+          sec = 59;
         } else
-          if ($scope.counter>=countTo) {
-            sec=0;
-            min=0;
-          } else {
-            angle+=inc;
-            $scope.counter++;
-            sec--;
-          }
+        if ($scope.counter >= $scope.countTo) {
+          sec = 0;
+          min = 0;
+        } else {
+          angle += inc;
+          $scope.counter++;
+          sec--;
+        }
       }
 
-      setInterval(drawScreen,1000);
+      // console.log("" + $scope.counter +  "  " + $scope.counToTime);
+      //       if ($scope.counter >= $scope.counToTime){
+      //         $scope.show = false;
+      //         alert("asdasdasd");
+      // }
+      setInterval(drawScreen, 1000);
 
     });
 
+
+    $scope.loadAndPopulate = function() {
+
+    }
 
     //Loading Maths List Items
     $http.get('cat-math.json').then(function(mathData) {
@@ -354,6 +377,21 @@
     //
     // }
 
+
+    $scope.xyz = function() {
+
+      if($scope.counter >= $scope.counToTime ){
+        return 'active';
+        alert("sdfd");
+      }
+
+      if (($scope.totalQuestions === $scope.activeQuestion))
+        return 'active';
+      else {
+        return 'inactive';
+      }
+    }
+
     $scope.selectAnswer = function(qIndex, aIndex) {
 
       var questionState = $scope.simpleJSON[qIndex].questionState;
@@ -377,6 +415,12 @@
 
     }
 
+    // $scope.displayResults = function() {
+    //   if ($scope.counter >=0 && $scope.counter <= $scope.countTo)
+    //     return true;
+    //   else
+    //     return false;
+    // }
 
     $scope.isSelected = function(qIndex, aIndex) {
       return ($scope.simpleJSON[qIndex].selectedAnswer === aIndex);
